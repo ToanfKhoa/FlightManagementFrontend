d360130f45cb471ef263d4d349d2262a9139517c
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-12-31 15:56:42 +0700

    feat: implement passenger registration

--- src/types/passengerType.ts
+++ src/types/passengerType.ts
@@ -1,10 +1,16 @@
+import { RegisterRequest } from "./authType";
+import { PageResponse } from "./commonType";
+
 export interface Passenger {
-    id: number;
+    accountRequest: RegisterRequest;
+    id?: number;
     fullName: string;
     dateOfBirth: string;
     nationality: string;
     idNumber: string;
+    address: string;
     phone: string;
 }
 
+export type PassengersPageResponse = PageResponse<Passenger>;
 export default Passenger;
